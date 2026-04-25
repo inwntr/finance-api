5ab572681376d74210506bd8cb195774b01b1c28
Winter ❄ <fxrstzinho@gmail.com>
2026-04-25 12:44:22 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,7 +12,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://haonfinance.netlify.app",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/auth", authRoutes);
