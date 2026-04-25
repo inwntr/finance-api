59b16a9608ce1b4ad8d0d5ebecc8b6cd397471d0
Winter ❄ <fxrstzinho@gmail.com>
2026-04-25 13:10:47 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,12 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 app.use(express.json());
 
