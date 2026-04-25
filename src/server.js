b5710e0a92ce4fe5c13201fd65f940fa3b2b29d2
Winter ❄ <fxrstzinho@gmail.com>
2026-04-25 13:03:20 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,13 +12,25 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  "https://haonfinance.netlify.app",
+  "http://localhost:5173"
+];
+
 app.use(cors({
-  origin: [
-    "https://haonfinance.netlify.app",
-    "http://localhost:5173"
-  ],
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options(/.*/, cors());
 
 app.use(express.json());
 
