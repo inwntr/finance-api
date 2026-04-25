66f578463eea78f7ad14d2fb9ae74f3f883d88f0
Winter ❄ <fxrstzinho@gmail.com>
2026-04-25 13:24:14 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -21,9 +21,14 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://haonfinance.netlify.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
   }
+
   next();
 });
 
